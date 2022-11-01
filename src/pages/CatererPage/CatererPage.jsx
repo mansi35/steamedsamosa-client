4d922af1cb555da87ac5f4f77617f61b8e0465fb
Mansi Sharma <mansi.isha.mansi@gmail.com>
2022-11-02 01:29:22 +0530

    add product price card

--- src/pages/CatererPage/CatererPage.jsx
+++ src/pages/CatererPage/CatererPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import ImagesBanner from '../../components/ImagesBanner/ImagesBanner';
+import ProductPriceCard from '../../components/ProductPriceCard/ProductPriceCard';
 import './CatererPage.scss';
 
 function CatererPage() {
   return (
     <div className="caterarpage">
       <ImagesBanner />
+      <div className="catererpage__desc">
+        <div className="catererpage__menu">
+          Menu
+        </div>
+        <div className="catererpage__order">
+          <ProductPriceCard />
+        </div>
+      </div>
     </div>
   );
 }
