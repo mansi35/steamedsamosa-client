8f6e7cf027d22ed1179c16727ad8238da8abf7a7
Aditi Tiwari <adititiwari@192.168.1.5>
2022-11-03 11:58:57 +0530

    Address details card added

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,31 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
     'max-len': 'off',
     'no-console': 'off',
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
   },
 };
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Address from '../../components/Address/Address';
+import CartProgressBar from '../../components/CartProgressBar/CartProgressBar';
+import OrderSummary from '../../components/OrderSummary/OrderSummary';
+import ShoppingCart from '../../components/ShoppingCart/ShoppingCart';
 
 function Cart() {
-  return <div>Cart</div>;
+  return (
+    <div>
+      <CartProgressBar />
+      <Address />
+      <OrderSummary />
+      <ShoppingCart />
+    </div>
+  );
 }
 
 export default Cart;
