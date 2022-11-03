6b866ebdfc9303efccbf3c3f752f880d2f69a21b
Shruthi Rao <raoshruthi2001@gmail.com>
2022-11-03 10:48:40 +0530

    Added more bookings and styling for them, added profile img padding

--- src/components/PastBookings/PastBookings.jsx
+++ src/components/PastBookings/PastBookings.jsx
@@ -4,27 +4,65 @@ import './PastBookings.scss';
 
 function PastBookings() {
   return (
-    <div>
+    <div className="pastbookings">
       <div className="bookingheading">
         <div className="bookingheading__heading">
           <h3>Past Bookings</h3>
         </div>
       </div>
-      <div className="bookingcard">
-        <div className="bookingcard__column">
-          <img className="bookingImg" src={babyshower} alt="generic_profile" />
+      <div className="bookingcards">
+        <div className="bookingcard">
+          <div className="bookingcard__column">
+            <img className="bookingImg" src={babyshower} alt="generic_profile" />
+          </div>
+          <div className="bookingcard__column">
+            <h4>Little Feet Baby Shower</h4>
+            <p>
+              8th March 2022
+              {' '}
+              <br />
+              Contact Details: 123456789
+              <br />
+            </p>
+            <br />
+            <button type="button">Cancel Booking</button>
+          </div>
         </div>
-        <div className="bookingcard__column">
-          <h4>Little Feet Baby Shower</h4>
-          <p>
-            8th March 2022
-            {' '}
+
+        <div className="bookingcard">
+          <div className="bookingcard__column">
+            <img className="bookingImg" src={babyshower} alt="generic_profile" />
+          </div>
+          <div className="bookingcard__column">
+            <h4>Little Feet Baby Shower</h4>
+            <p>
+              8th March 2022
+              {' '}
+              <br />
+              Contact Details: 123456789
+              <br />
+            </p>
             <br />
-            Contact Details: 123456789
+            <button type="button">Cancel Booking</button>
+          </div>
+        </div>
+
+        <div className="bookingcard">
+          <div className="bookingcard__column">
+            <img className="bookingImg" src={babyshower} alt="generic_profile" />
+          </div>
+          <div className="bookingcard__column">
+            <h4>Little Feet Baby Shower</h4>
+            <p>
+              8th March 2022
+              {' '}
+              <br />
+              Contact Details: 123456789
+              <br />
+            </p>
             <br />
-          </p>
-          <br />
-          <button type="button">Cancel Booking</button>
+            <button type="button">Cancel Booking</button>
+          </div>
         </div>
       </div>
       <div className="loadMore">
--- src/components/UpcomingBookings/UpcomingBookings.jsx
+++ src/components/UpcomingBookings/UpcomingBookings.jsx
@@ -4,23 +4,66 @@ import './UpcomingBookings.scss';
 
 function UpcomingBookings() {
   return (
-    <div>
-      <div className="bookingheading"><div className="bookingheading__heading"><h3>Upcoming Bookings</h3></div></div>
-      <div className="bookingcard">
-        <div className="bookingcard__column">
-          <img className="bookingImg" src={babyshower} alt="generic_profile" />
+    <div className="upcomingbookings">
+      <div className="bookingheading">
+        <div className="bookingheading__heading">
+          <h3>Upcoming Bookings</h3>
         </div>
-        <div className="bookingcard__column">
-          <h4>Little Feet Baby Shower</h4>
-          <p>
-            8th March 2022
-            {' '}
+      </div>
+
+      <div className="bookingcards">
+        <div className="bookingcard">
+          <div className="bookingcard__column">
+            <img className="bookingImg" src={babyshower} alt="generic_profile" />
+          </div>
+          <div className="bookingcard__column">
+            <h4>Little Feet Baby Shower</h4>
+            <p>
+              8th March 2022
+              {' '}
+              <br />
+              Contact Details: 123456789
+              <br />
+            </p>
             <br />
-            Contact Details: 123456789
+            <button type="button">Cancel Booking</button>
+          </div>
+        </div>
+
+        <div className="bookingcard">
+          <div className="bookingcard__column">
+            <img className="bookingImg" src={babyshower} alt="generic_profile" />
+          </div>
+          <div className="bookingcard__column">
+            <h4>Little Feet Baby Shower</h4>
+            <p>
+              8th March 2022
+              {' '}
+              <br />
+              Contact Details: 123456789
+              <br />
+            </p>
             <br />
-          </p>
-          <br />
-          <button type="button">Cancel Booking</button>
+            <button type="button">Cancel Booking</button>
+          </div>
+        </div>
+
+        <div className="bookingcard">
+          <div className="bookingcard__column">
+            <img className="bookingImg" src={babyshower} alt="generic_profile" />
+          </div>
+          <div className="bookingcard__column">
+            <h4>Little Feet Baby Shower</h4>
+            <p>
+              8th March 2022
+              {' '}
+              <br />
+              Contact Details: 123456789
+              <br />
+            </p>
+            <br />
+            <button type="button">Cancel Booking</button>
+          </div>
         </div>
       </div>
     </div>
