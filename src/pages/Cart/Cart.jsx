1adf055f17907dd227a118f3f7d2c6c67f1c24ea
Aditi Tiwari <adititiwari@192.168.1.2>
2022-11-04 11:08:46 +0530

    Order Summary component added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -3,13 +3,20 @@ import Address from '../../components/Address/Address';
 import CartProgressBar from '../../components/CartProgressBar/CartProgressBar';
 import OrderSummary from '../../components/OrderSummary/OrderSummary';
 import ShoppingCart from '../../components/ShoppingCart/ShoppingCart';
+import './Cart.scss';
 
 function Cart() {
   return (
-    <div>
+    <div className="cart__container">
       <CartProgressBar />
-      <Address />
-      <OrderSummary />
+      <div className="cart__address__order">
+        <div className="big__item">
+          <Address />
+        </div>
+        <div className="small__item">
+          <OrderSummary />
+        </div>
+      </div>
       <ShoppingCart />
     </div>
   );
