3d3f60c1a353e65313ee6018df8369c4b7dabaa6
sukriti <sukritidawar@gmail.com>
2022-11-06 10:02:19 +0530

    Auth

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,13 @@ import Caterer from './pages/Caterer/Caterer';
 import Florist from './pages/Florist/Florist';
 import Cart from './pages/Cart/Cart';
 import {
-  getAccomodations, getBeauticians, getCaterers, getFlorists, getMusicOptions, getPhotographers, getVenues,
+  getAccomodations,
+  getBeauticians,
+  getCaterers,
+  getFlorists,
+  getMusicOptions,
+  getPhotographers,
+  getVenues,
 } from './actions/events';
 import { getPackages } from './actions/packages';
 import CatererList from './pages/CatererList/CatererList';
@@ -46,9 +52,17 @@ function App() {
             <Route exact path="/caterer" element={<Caterer />} />
             <Route exact path="/florist" element={<Florist />} />
             <Route exact path="/caterers" element={<CatererList />} />
-            <Route exact path="/products/:productid" element={<ProductDescription />} />
+            <Route
+              exact
+              path="/products/:productid"
+              element={<ProductDescription />}
+            />
             <Route exact path="/packages" element={<PackagesList />} />
-            <Route exact path="/packages/:packageid" element={<PackageDescription />} />
+            <Route
+              exact
+              path="/packages/:packageid"
+              element={<PackageDescription />}
+            />
             <Route exact path="/profile" element={<Profile />} />
             <Route exact path="/cart" element={<Cart />} />
           </Routes>
